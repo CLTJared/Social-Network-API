dcc85c86fc3e403c4a11dd6b76e7f30da9619439
Jared Elliott <bootcamp@jaredclt.com>
2024-02-13 20:38:38 -0500

    Created initial seed of users.

---
+++ seed/index.js
@@ -0,0 +1,36 @@
+const { User, Thought } = require("../models");
+const mongoose = require("mongoose");
+
+const connection = require("../config/connection");
+
+// Seed data for creating users
+  const users = [
+    {
+      username: "JaredUNCC",
+      thoughts: [],
+      friends: []
+    },
+    {
+      username: "RogerEDX",
+      thoughts: [],
+      friends: []
+    },
+    {
+      username: "SashaEDX",
+      thoughts: [],
+      friends: []
+    },
+  ];
+
+console.log(connection);
+
+connection.once("open", async () => {
+  console.log("connected");
+
+  await User.deleteMany({});                // Delete any existing User records so we don't have issues
+  await User.collection.insertMany(users);  // Create users from `users` array above
+
+  console.table(users);
+  console.info("Seeding complete...");
+  process.exit(0);
+});
